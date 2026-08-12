762b48f7a17d12ac4ab9bfe1692a0ca84ed4e75d
matthewcavanaugh96 <matthew.cavanaugh.96@gmail.com>
2026-08-12 11:30:34 -0700

    Added visualizations and refinements

--- stock_prices_app/stock_prices_5_mod_results.py
+++ stock_prices_app/stock_prices_5_mod_results.py
@@ -15,11 +15,11 @@ tab1, tab2 = st.tabs([
 with tab1:
     st.header("Ensemble Round 1")
     st.write(""" 
-    So far, this includes: Linear Regression, XGBoost, Random Forest, Extra Trees, Gradient Booster.
+    So far, this includes: Linear Regression, XGBoost, Random Forest, Extra Trees, Gradient Booster. These are the five models I initially ran for feature engineering purposes, though more may be added to this ensemble.
     """)
 
     #st.image("visualizations/Round_1_Model_Aggs.png", caption="Individual model aggregates", width=600)
-    st.image("visualizations/models_and_ensemble.png", caption="Individual and ensemble scores. NOTE: Ensemble metrics are not a simple average of individual model metrics; they are calculated separately from the ensemble’s averaged predictions. This is how the some metrics are able to improve.", width=800)
+    st.image("visualizations/models_and_ensemble.png", caption="Individual and ensemble scores. NOTE: Ensemble metrics are not a simple average of individual model metrics; they are calculated separately from the ensemble’s averaged predictions. This is why some metrics improve and others may decline.", width=800)
 
 
 
--- stock_prices_app/stock_prices_7_failure.py
+++ stock_prices_app/stock_prices_7_failure.py
@@ -25,7 +25,7 @@ What if you took the model's advice every day?
 """)
 
 st.write("""
-I simulated a scenario in which an investor took the models' advice every day in 2025. This hypothetical investor knows nothing other than the models' aggregate directional prediction for the day, and will act accordingly at open. They will enter day one of the simulation holding a single share of a stock at the previous day's actual closing value. For this and all subsequent days, their decision to buy, hold, or sell will be determined by the ensemble model's directional guess. If the ensemble predicts a positive movement, they will either hold their share or buy one if they don't have one. If the ensemble predicts a negative movement, they will sell their share if they have one or do nothing if they don't.
+I simulated a scenario in which an investor took the models' advice every day in 2025. This hypothetical investor knows nothing other than the models' aggregate directional prediction for the day, and will act accordingly at open. They will enter day one of the simulation holding a single share of a stock at the previous day's actual closing value. For this and all subsequent days, their decision to buy, hold, or sell will be determined by the ensemble model's directional guess. If the ensemble predicts a positive movement, they will either hold their share if they have one or buy one if they don't have. If the ensemble predicts a negative movement, they will sell their share if they have one or do nothing if they don't.
 """)
 
 
@@ -35,23 +35,27 @@ st.write("""
 This suggested to me that the average positive day probably produces a stronger growth than a negative day does a loss. But even this difference was surprisingly negligible: positive days had a mean growth of 2.026 and median of 1.238, while negative days had a mean loss of -1.927 and median of -1.232, and there were slightly more negative than positive stock days.
 """)
 
-# st.image("visualizations/v20_ensemble_direct_correct.png", caption="Picture unrelated", width=200)
-# st.image("visualizations/v21_ensemble_better_than_holding.png", caption="Caption", width=200)
+st.write("""
+Below, see which stocks would have done the best and worst for an investor following the model's advice.
+""")
+
+st.image("visualizations/ensemble_sim_top_10_pct.png", caption="These ten stocks had the largest potential return, by percentage, for an investor who always followed the ensemble model's advice.")
+
+st.image("visualizations/ensemble_sim_bottom_10_pct.png", caption="These ten stocks had the largest potential loss, by percentage, for an investor who always followed the ensemble model's advice.")
+
+
 
 
 # Correct every day
-# Wrong every day
 st.header("""
-Nostradamus?
+What if you could be right every day?
 """)
 
 st.write("""
-For fun, let's assume you have a crystal ball and can predict directional accuracy every day - perfectly holding, buying, or selling in anticipation of the coming day.
-
-If movement was positive, you'll be rewarded by the gain.
-
-If it was negative, you'll take no loss since you did not hold the stock.
+For fun, I also simulated an investor who had a crystal ball and could perfectly predict directional accuracy - correctly holding, buying, or selling in anticipation of the coming day. If movement was positive, they'll be rewarded by the gain. If it was negative, they'll take no loss since they sold at opening.
 """)
+
+st.image("visualizations/always_right_top_10_pct.png", caption="These ten stocks hold the largest potential return by percentage for an investor who made the correct decision every day.")
 
 
 # Wrong every day
@@ -59,9 +63,11 @@ st.header("""
 What if you made the wrong decision every day?
 """)
 
-
 st.write("""
 What about the other extreme - an investor who makes the WRONG decision every day?
 """)
 
 st.image("media/cramer.jpg", caption="Picture unrelated", width=200)
+
+st.image("visualizations/always_wrong_top_10_pct.png", caption="These ten stocks hold the largest potential loss by percentage for an investor who made the wrong decision every day.")
+
